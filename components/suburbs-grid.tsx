1c0af1eb342141a0b40b491b22123b88f13f480a
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2026-01-02 21:44:03 +0200

    Make SuburbsGrid props optional with defaults

--- components/suburbs-grid.tsx
+++ components/suburbs-grid.tsx
@@ -1,11 +1,43 @@
+// Option 1: Pass the required props when using SuburbsGrid
+// In your generator-repairs-near-me/page.tsx, replace line 412:
+
+<SuburbsGrid 
+  title="Areas We Service Near You"
+  suburbs={[
+    "Sandton", "Johannesburg CBD", "Randburg", "Roodepoort",
+    "Fourways", "Midrand", "Pretoria East", "Centurion",
+    "Pretoria North", "Pretoria West", "Kempton Park", "Boksburg",
+    "Benoni", "Germiston", "Edenvale", "Bedfordview",
+    "Bryanston", "Rosebank", "Parktown", "Houghton",
+    "Melrose", "Illovo", "Hyde Park", "Dunkeld",
+    "Craighall", "Northcliff", "Florida", "Honeydew",
+    "Douglasdale", "Lonehill", "Sunninghill", "Woodmead"
+  ]}
+/>
+
+// OR Option 2: Make props optional in SuburbsGrid component
+// Update components/suburbs-grid.tsx:
+
 import { MapPin } from "lucide-react"
 
 interface SuburbsGridProps {
-  title: string
-  suburbs: string[]
+  title?: string
+  suburbs?: string[]
 }
 
-export function SuburbsGrid({ title, suburbs }: SuburbsGridProps) {
+export function SuburbsGrid({ 
+  title = "Areas We Service Near You", 
+  suburbs = [
+    "Sandton", "Johannesburg CBD", "Randburg", "Roodepoort",
+    "Fourways", "Midrand", "Pretoria East", "Centurion",
+    "Pretoria North", "Pretoria West", "Kempton Park", "Boksburg",
+    "Benoni", "Germiston", "Edenvale", "Bedfordview",
+    "Bryanston", "Rosebank", "Parktown", "Houghton",
+    "Melrose", "Illovo", "Hyde Park", "Dunkeld",
+    "Craighall", "Northcliff", "Florida", "Honeydew",
+    "Douglasdale", "Lonehill", "Sunninghill", "Woodmead"
+  ]
+}: SuburbsGridProps) {
   return (
     <section className="py-12 bg-muted/30">
       <div className="container mx-auto px-4">
